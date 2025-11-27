47084c65615bca3f8bf926ade61632b9c77bca3a
Nathaniel-Rabaya-CSUB <nrabaya@csub.edu>
2025-11-26 18:28:55 -0800

    Added calendar with randomized date and WIP game over screen

--- CMPS 3390 P3/Assets/Scripts/UI/MainMenu.cs
+++ CMPS 3390 P3/Assets/Scripts/UI/MainMenu.cs
@@ -11,7 +11,7 @@ public class MainMenu : MonoBehaviour
     public void OpenLeaderboard()
     {
         Debug.Log("URL OPENED");
-        Application.OpenURL("https://cs.csub.edu/~slara/3390/Project3/leaderboard/");
+        Application.OpenURL("https://cs.csub.edu/~slara/3390/Project3/leaderboard/frontend/");
     }
 
     public void QuitGame()
